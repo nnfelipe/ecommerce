1b8addabb3b10b7cebd72de0bc4f6461b61f2ca6
nnfelipe.fe <felipe.finsterbusch@outlook.com>
2015-03-20 10:16:58 -0300

    crear el modelo de catalogo produto por categoria y agregué vistas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316134417) do
+ActiveRecord::Schema.define(version: 20150317162654) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150316134417) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "price"
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
